80f4fdb6c41327fb8c6a3e2e4469561001aea862
Evgenia Tarasova <etarasova@live.ru>
2023-03-17 13:22:47 -0700

    import from csv

---
+++ api/v1/urls.py
@@ -0,0 +1,35 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from reviews.views import (
+    CategoryViewSet,
+    CommentViewSet,
+    GenreViewSet,
+    ReviewViewSet,
+    TitleViewSet,
+)
+from users.views import UserSignUpAPIView, UserViewSet, token_obtain
+
+v1_router = DefaultRouter()
+v1_router.register(r'users', UserViewSet, basename='users')
+
+v1_router.register(r'categories', CategoryViewSet, basename='categories')
+v1_router.register(r'genres', GenreViewSet, basename='genres')
+v1_router.register(r'titles', TitleViewSet, basename='titles')
+v1_router.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews'
+)
+v1_router.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
+
+
+urlpatterns = [
+    path('', include(v1_router.urls), name='api-root'),
+    path('auth/signup/', UserSignUpAPIView.as_view(), name='signup'),
+    path('auth/token/', token_obtain, name='token'),
+]
